9a65bb6bde88ac37a3c4da2057185335ab551f2d
AlexanderCalle <alexander.callebaut2@student.hogent.be>
2024-10-21 21:02:56 +0200

    Hover ddd post underline

--- src/components/Dairy/DairyItem.jsx
+++ src/components/Dairy/DairyItem.jsx
@@ -5,7 +5,7 @@ const DiaryItem = ({item}) => {
   return (
     <a
         href={`/diary/${item.id}`}
-        className={`h-fit relative flex flex-col gap-2 mb-4 before:content-[''] before:rounded-md before:absolute before:inset-0 break-inside-avoid-column`}
+        className={`h-fit relative flex flex-col gap-2 mb-4 before:content-[''] before:rounded-md before:absolute before:inset-0 break-inside-avoid-column group`}
         style={{backgroundColor: item.bg_color}}
     >
         {item.image !== "" &&  (
@@ -20,7 +20,7 @@ const DiaryItem = ({item}) => {
         <div className={`p-2  ${
             item.isInvert && "[&_*:not(image)]:invert"
         }`}>
-            <h3 className='font-bold text-lg'>{item.title}</h3>
+            <h3 className='font-bold group-hover:underline text-lg'>{item.title}</h3>
             {item.description !== "" && <p>{item.description}</p>}
             {item.author !== "" && <p className='text-sm font-light'>{item.author}</p>}
         </div>
