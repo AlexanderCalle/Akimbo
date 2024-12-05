54326e2f39ad579822039dd91ef8bf9a17cf86db
AlexanderCalle <alexander.callebaut2@student.hogent.be>
2024-12-05 21:04:50 +0100

    Responsive design bug fixes

--- src/components/Articles/RecentPost.jsx
+++ src/components/Articles/RecentPost.jsx
@@ -6,17 +6,15 @@ const RecentPost = ({ article, idx }) => {
 
 
   return (
-    <section
-      className={`w-full h-full lg:w-96 flex flex-col gap-4 items-start justify-start`}
-    >
+    <section className="max-w-96 flex flex-col gap-4 items-start justify-start">
       <div className="self-start font-sans m-1 px-3 py-1 absolute bg-akimbo-light bg-opacity-70 lowercase">{article.type === TYPE_ARTICLE ? article.cat : article.type}</div>
-      {article.image !== "" ? <img
+       {article.image !== "" ? <img
         src={article.image}
         alt={article.imageTitle}
-        className="w-full min-h-[150px] h-3/6 aspect-square object-cover bg-center"
-      /> : <div className="w-full h-3/6 aspect-square object-cover bg-center" style={{backgroundColor: article.bg_color}} />}
+        className="w-full min-h-[150px] h-2/6 object-cover bg-center"
+      /> : <div className="w-full h-3/6 object-cover bg-center" style={{backgroundColor: article.bg_color}} />}
       <article
-        className={`h-full flex flex-col p-2 items-start gap-2 bg-akimbo-light bg-opacity-80 backdrop-blur-sm`}
+        className={`h-96 flex flex-col p-2 items-start justify-between gap-2 bg-akimbo-light bg-opacity-80 backdrop-blur-sm`}
       >
         <div className={`flex flex-col gap-2 items-start`}>
           <div className="flex gap-2 items-center h-7">
@@ -44,7 +42,7 @@ const RecentPost = ({ article, idx }) => {
           className={`flex-grow w-full max-h-48 text-ellipsis overflow-hidden  text-start`}
         >{article.description}</p>
         <button
-          className="bg-akimbo-dark-900 px-3 py-2 text-akimbo-light"
+          className="bg-akimbo-dark-900 px-3 py-2 text-akimbo-light justify-self-end"
           onClick={() => {
             navigate(article.type === TYPE_ARTICLE ? `/articles/${article.cat}/${article.id}` : `/diary/${article.id}`);
           }}
