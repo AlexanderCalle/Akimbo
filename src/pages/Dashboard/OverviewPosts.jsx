069c46a0436034f14e959d128179703299c010da
AlexanderCalle <alexander.callebaut2@student.hogent.be>
2025-10-05 18:13:46 +0200

    feat: add analytics plausible to dashboard page

--- src/pages/Dashboard/OverviewPosts.jsx
+++ src/pages/Dashboard/OverviewPosts.jsx
@@ -8,6 +8,16 @@ const OverviewPosts = () => {
   return (
     <div>
       <PlannedArticles />
+      <iframe 
+        title="Plausible Analytics Dashboard"
+        plausible-embed 
+        src="https://plausible.akimbo-mag.com/share/akimbo-mag.com?auth=4-mf1ikWA8NrwRYpZFpsd&embed=true&theme=light&background=transparent" 
+        frameborder="0"  
+        loading="lazy" 
+        scrolling="no"
+        style={{width: "1px", minWidth: "100%", height: "1500px", overflow: "hidden"}} 
+      />
+        <script async src="https://plausible.akimbo-mag.com/js/embed.host.js"></script>
       {/* <OverviewArticles /> */}
       {/* <OverviewDairyItems /> */}
     </div>
